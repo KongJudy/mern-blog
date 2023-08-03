2ac2c80ad2ced00384d8c5d0adbd3621786de9b8
Judy Kong <judy.kong817@gmail.com>
2023-08-03 15:45:46 -0400

    register user form added to mongodb

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,19 @@
 import Header from './components/Header';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className='App bg-classic-gray min-h-screen'>
-        <Header />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div className='App bg-classic-gray min-h-screen'>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
+    </div>
   );
 }
 
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
 
-const InputItem = ({ children, type, value, onChange }) => {
+const InputItem = ({ children, type, name, value, onChange }) => {
   return (
     <div className='text-left md:text-right'>
       <label className='block my-2 md:inline-block font-bold'>{children}</label>
       <input
         className='mb-4 p-2 md:mx-3 rounded h-10 w-full md:w-80 hover:drop-shadow-sm'
         type={type}
+        name={name}
         value={value}
         onChange={onChange}
       />
@@ -17,46 +19,101 @@ const InputItem = ({ children, type, value, onChange }) => {
 };
 
 const Register = () => {
-  const [email, setEmail] = useState('');
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+  const [inputValues, setInputValues] = useState({
+    email: '',
+    username: '',
+    password: ''
+  });
 
-  const handleSubmitForm = (e) => {
+  const { email, username, password } = inputValues;
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setInputValues({
+      ...inputValues,
+      [name]: value
+    });
+  };
+
+  // TOAST SUCCESS/ERROR MESSAGE
+  const handleError = (err) => {
+    toast.error(err, {
+      position: 'bottom-right'
+    });
+  };
+
+  const handleSuccess = (msg) => {
+    toast.success(msg, {
+      position: 'bottom-right'
+    });
+  };
+
+  // SUBMIT FORM HANDLER
+  const handleSubmitForm = async (e) => {
     e.preventDefault();
-    axios
-      .post('', { username, email, password })
-      .then((result) => console.log(result))
-      .catch((err) => console.log(err));
+    try {
+      const { data } = await axios.post(
+        'http://localhost:4000/register',
+        {
+          ...inputValues
+        },
+        { withCredentials: true }
+      );
+      const { success, message } = data;
+      if (success) {
+        handleSuccess(message);
+        setTimeout(() => {
+          navigate('/');
+        }, 1000);
+      } else {
+        handleError(message);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+    setInputValues({
+      ...inputValues,
+      email: '',
+      username: '',
+      password: ''
+    });
   };
 
   return (
     <div className='w-full mx-auto pt-6'>
       <div className='mt-40 flex justify-center'>
         <div className='md:p-8 md:rounded md:shadow-2xl'>
-          <form onSubmit={() => handleSubmitForm}>
+          <form onSubmit={handleSubmitForm}>
             <div className='text-center mb-12'>
               <span className='text-3xl font-bold'>Register</span>
             </div>
             <InputItem
               children='Email:'
               type='email'
+              name='email'
               value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              onChange={handleOnChange}
             />
             <InputItem
               children='Username:'
               type='username'
+              name='username'
               value={username}
-              onChange={(e) => setUsername(e.target.value)}
+              onChange={handleOnChange}
             />
             <InputItem
               children='Password:'
               type='password'
+              name='password'
               value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              onChange={handleOnChange}
             />
             <div className='text-right md:mr-3'>
-              <button className='bg-white px-4 py-2 rounded hover:font-bold tracking-wider shadow-md hover:drop-shadow-sm'>
+              <button
+                type='submit'
+                className='bg-white px-4 py-2 rounded hover:font-bold tracking-wider shadow-md hover:drop-shadow-sm'
+              >
                 Register
               </button>
               <div className='pt-4'>
@@ -69,6 +126,7 @@ const Register = () => {
               </div>
             </div>
           </form>
+          <ToastContainer />
         </div>
       </div>
     </div>
