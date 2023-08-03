eb919bc5eb1a8a2faef9a8bb861bafa6de749b02
Judy Kong <judy.kong817@gmail.com>
2023-08-03 13:20:20 -0400

    user has access to main route

--- backend/routes/AuthRoute.js
+++ backend/routes/AuthRoute.js
@@ -1,6 +1,8 @@
 const { Register, Login } = require('../controllers/AuthController');
+const { userVerification } = require('../middlewares/AuthMiddleware');
 const router = require('express').Router();
 
+router.post('/', userVerification);
 router.post('/register', Register);
 router.post('/login', Login);
 
